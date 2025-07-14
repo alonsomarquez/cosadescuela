c6d41aba0d0afe2ce65762be1424ae5551e5f35e
EnmanuelParedes007 <tresmilpe@gmail.com>
2025-07-14 15:18:42 -0400

    moricion

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const form = document.querySelector(".form-create")
-
+let cont = 0;
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const post = document.querySelector("#post").value;
     const safe = document.querySelector(".post-content");
     safe.innerHTML += `<div class="post">
-            <p>id: 00</p>
+            <p>id: ${cont}</p>
             <p>${post}</p>
             <button>editar</button>
             <button>eliminar</button>
-            <p>meow meow</p>
         </div>`;
-})
+        cont++;
+});
